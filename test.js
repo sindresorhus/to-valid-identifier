eeec66a32695028c6ad08303f1e5c526e29043d2
Brett Zamir <brettz9@yahoo.com>
2025-10-14 11:51:34 -0700

    fix: escape non-ID_Start characters (e.g., digits) in initial position

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ import toValidIdentifier from './index.js';
 test('main', t => {
 	t.is(toValidIdentifier('foo'), 'foo');
 	t.is(toValidIdentifier('$foo'), '$a$foo');
+	t.is(toValidIdentifier('_foo'), '$1X$foo');
+	t.is(toValidIdentifier('9foo'), '$v$foo');
 	t.is(toValidIdentifier('foo-bar'), 'foo$j$bar');
 	t.is(toValidIdentifier('null'), '$_null$');
 	t.is(toValidIdentifier('undefined'), '$_undefined$');
